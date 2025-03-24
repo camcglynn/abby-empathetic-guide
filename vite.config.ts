6c2f71cef41ba29fc9f74575bae5963ffb27c558
Chloe McGlynn <143466924+camcglynn@users.noreply.github.com>
2025-03-24 14:14:59 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/camcglynn/abby-empathetic-guide/' : '/',
+  base: mode === 'production' ? '/abby-empathetic-guide/' : '/',
   server: {
     host: "::",
     port: 8080,
